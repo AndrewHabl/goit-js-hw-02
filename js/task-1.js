0c149cbc72b4d877c153741beea8b5fa2fb7aff7
Habl <habltaker@gmail.com>
2025-05-15 16:28:41 +0300

    1 task

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity*pricePerDroid;
+    if (customerCredits < totalPrice) { return "Insufficient funds!" }
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
